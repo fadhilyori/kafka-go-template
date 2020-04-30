8558d2b576ccc76743be9ae84ddbc0cc51f5dce3
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2020-04-30 21:37:19 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+
+	const (
+		PollTimeout           int = 100
+		KafkaSessionTimeoutMs int = 6000
+	)
+
+	if len(os.Args) < 4 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <broker> <group> <topics..>\n",
+			os.Args[0])
+		os.Exit(1)
+	}
+
+	broker := os.Args[1]
+	group := os.Args[2]
+	topics := os.Args[3:]
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	kafkaConnection, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":     broker,
+		"broker.address.family": "v4",
+		"group.id":              group,
+		"session.timeout.ms":    KafkaSessionTimeoutMs,
+		"auto.offset.reset":     "latest"})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create consumer: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Created Consumer %v\n", kafkaConnection)
+
+	err = kafkaConnection.SubscribeTopics(topics, nil)
+
+	run := true
+
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev := kafkaConnection.Poll(PollTimeout)
+			if ev == nil {
+				continue
+			}
+
+			switch e := ev.(type) {
+			case *kafka.Message:
+				fmt.Printf("%% Incoming Message on %s:\n%s\n", e.TopicPartition, string(e.Value))
+
+				/*
+					TODO:
+					Do parsing and processing data here
+				*/
+
+				if e.Headers != nil {
+					fmt.Printf("%% Headers: %v\n", e.Headers)
+				}
+			case kafka.Error:
+				fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
+				if e.Code() == kafka.ErrAllBrokersDown {
+					run = false
+				}
+			default:
+				/*
+					TODO:
+					Here you can also run the data processing and sending data to Database
+				*/
+			}
+		}
+	}
+
+	fmt.Printf("Closing consumer\n")
+	kafkaConnection.Close()
+}
